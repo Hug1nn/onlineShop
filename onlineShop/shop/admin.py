7e9e58572a371e605da4e710637613bb821f8c7c
Hug1nn <ukhachevych@icloud.com>
2019-01-17 21:33:35 +0200

    add Product and Category  models

--- onlineShop/shop/admin.py
+++ onlineShop/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'description', 'image']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'description', 'price']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('name',)}
+
--- onlineShop/shop/models.py
+++ onlineShop/shop/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.urls import reverse
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, db_index=True, default='category')
     description = models.TextField(blank=True)
-    image = models.BinaryField(editable=True)
+    image = models.BinaryField(editable=True, blank=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -23,11 +24,14 @@ class Product(models.Model):
         Category,
         related_name='products',
         on_delete = 'cascade')
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, db_index=True, default='product')
     description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
         ordering = ('name',)
+        index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
